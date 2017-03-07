2d871f436b8cab3616b7dece04538a6cfc1e79a3
satsargsyan <satenik.sargsyan2017@gmail.com>
2017-03-08 01:28:23 +0400

    4 examples

--- Ref_vs_Out/Ref_vs_Out/Program.cs
+++ Ref_vs_Out/Ref_vs_Out/Program.cs
@@ -10,20 +10,32 @@ namespace Ref_vs_Out
     {
         static void Main(string[] args)
         {
-            //int i = 3; // Variable need to be initialized 
-            //Console.WriteLine("before calling method ref {0}", i);
-            //sampleMethod_for_ref(ref i);
-            //Console.WriteLine("after calling method ref {0}",i);
+            int i = 3; // Variable need to be initialized 
+            Console.WriteLine("before calling method ref: {0}", i);
+            sampleMethod_for_ref(ref i);
+            Console.WriteLine("after calling method ref: {0}", i);
 
-            //int k, j; // Variable needn't be initialized 
-            //Console.WriteLine(sampleMethod_for_out(out k, out j)); 
-            //Console.WriteLine("after calling method out {0}, {1}", k,j);
+            Console.WriteLine();
+
+            int k, j; // Variable needn't be initialized 
+            Console.WriteLine("after calling  sampleMethod_for_out: {0}",  sampleMethod_for_out(out k, out j));
+            Console.WriteLine("after calling  sampleMethod_for_out k and j  variables passed values: k= {0}, j= {1}", k, j);
+
+            Console.WriteLine();
 
             int a = 5;
             int b = 15;
-            Console.WriteLine("before calling method swap {0}, {1}", a, b);
+            Console.WriteLine("before calling method swap: {0},  {1}", a, b);
             swap(ref a,ref b);
-            Console.WriteLine("after calling method swap {0}, {1}", a, b);
+            Console.WriteLine("after calling method swap: {0},  {1}", a, b);
+
+            Console.WriteLine();
+
+            string s1 = "Barev";
+            string s2 = "hayer";
+            Console.WriteLine("before calling method swap: {0}  {1}", s1, s2);
+            swap(ref s1, ref s2);
+            Console.WriteLine("after calling method swap: {0}  {1}", s1, s2);
 
             Console.ReadKey();
         }
@@ -41,6 +53,12 @@ namespace Ref_vs_Out
         public static void swap(ref int  a, ref int  b)
         {
             int t = a;
+            a = b;
+            b = t;
+        }
+        public static void swap(ref string a, ref string b)
+        {
+            string t = a;
             a = b;
             b = t;
         }
