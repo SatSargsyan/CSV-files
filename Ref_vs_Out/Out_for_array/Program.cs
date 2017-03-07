d5d8a2cf8d09fa55c76030282166227b73bec8b7
satsargsyan <satenik.sargsyan2017@gmail.com>
2017-03-07 23:58:34 +0400

    4 examples

---
+++ Ref_vs_Out/Out_for_array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_for_array
+{
+    class TestOut
+    {
+        static void FillArray(out int[] arr)
+        {
+            // Initialize the array:
+            arr = new int[5] { 1, 2, 3, 4, 5 };
+        }
+
+        static void Main()
+        {
+            int[] theArray; // Initialization is not required
+
+            // Pass the array to the callee using out:
+            FillArray(out theArray);
+
+            // Display the array elements:
+            Console.WriteLine("Array elements are:");
+            for (int i = 0; i < theArray.Length; i++)
+            {
+                Console.Write(theArray[i] + " ");
+            }
+
+            // Keep the console window open in debug mode.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ref_vs_Out/Ref_for_array/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_for_array
+{
+    class TestRef
+    {
+        static void FillArray(ref int[] arr)
+        {
+            // Create the array on demand:
+            if (arr == null)
+            {
+                arr = new int[10];
+            }
+            // Fill the array:
+            arr[0] = 1111;
+            arr[4] = 5555;
+        }
+
+        static void Main()
+        {
+            // Initialize the array:
+            int[] theArray = { 1, 2, 3, 4, 5 };
+
+            // Pass the array using ref:
+            FillArray(ref theArray);
+
+            // Display the updated array:
+            Console.WriteLine("Array elements are:");
+            for (int i = 0; i < theArray.Length; i++)
+            {
+                Console.Write(theArray[i] + " ");
+            }
+
+            // Keep the console window open in debug mode.
+                Console.ReadKey();
+        }
+    }
+   
+}
